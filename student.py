93bca55d0e477088caa090b9f7b79809d04eb661
Bruce101github <brucethiombiano@gmail.com>
2024-08-07 11:31:50 +0000

    Add Student class to handle student data

---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+    """A class to represent a student in the student management system"""
+
+    def __init__(self):
+        """Initialize student records"""
+        self.record = []
+   
+ 
